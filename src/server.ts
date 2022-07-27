67dc4c6be263020564848c47af45d3a0e7cdd8d3
mikekamau <11148811+MikeKamau@users.noreply.github.com>
2022-07-27 14:02:36 +0300

    Implement Endpoint

--- src/server.ts
+++ src/server.ts
@@ -13,23 +13,43 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async ( req: Request, res: Response ) => {
 
-  /**************************************************************************** */
-
-  //! END @TODO1
+    //retrieve the url from the image_url query parameter
+   let image_url = req.query.image_url;
+    
+   //Check to see if the image_url query parameter has been specified and a value provided for it
+   //If a value has been provided, then check to determine if the provided image_url is a valid url and not some other value
+   if ( !image_url ) {
+     return res.status(400)
+               .send(`A valid image URL is required e.g. http://${req.hostname}:8082/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg `);
+   } else {
+         try {
+           
+          const URL = require('url').URL;
+          let is_url_valid = new URL(image_url);
+          
+         } catch (error) {
+           return res.status(400)
+                     .send(`Please enter a valid URL as a parameter`);
+         }
+     }
+    
+   //Send file to filterImageFromURL then send back the filtered output to the user
+   
+   try {
+     let filteredfilepath = await filterImageFromURL(image_url);
+     return res.status(200)
+               .sendFile(filteredfilepath, () => {
+                       let filePathArray: string[] = [filteredfilepath];
+                       deleteLocalFiles(filePathArray);
+     });
+   } catch (error) {
+     return res.status(400)
+               .send('unable to filter content at image_url');
+   }
+ 
+   });
   
   // Root Endpoint
   // Displays a simple message to the user
